af2af0d78428cf5376bad89ed40decbc1a3674d3
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-09-05 21:47:15 -0700

    begin add bootstrap and css

--- server.py
+++ server.py
@@ -6,5 +6,17 @@ app.secret_key = 'commit often'
 def home():
     return render_template("index.html", session = session)
 
+@app.route('/store', methods=['POST'])
+def store():
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comments'] = request.form['comments']
+    return redirect('/results')
+
+@app.route('/results')
+def show_results():
+    return render_template('results.html')
+
 if __name__=="__main__":
     app.run(debug=True)
